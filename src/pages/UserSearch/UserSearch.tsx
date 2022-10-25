4e1dfb639342e04c3da4fd9d53bfa70a6e17ea53
gruvas <gruvas67@gmail.com>
2022-10-25 18:04:41 +0300

    Basic setup

---
+++ src/pages/UserSearch/UserSearch.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserSearch = () => {
+    return (
+        <div className='text-center'>
+            test
+        </div>
+    );
+}
+
+export default UserSearch;
